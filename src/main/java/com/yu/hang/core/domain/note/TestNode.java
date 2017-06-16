660f167b16fc999f3e2f383e1a6f5a4597df5ffc
yuhang <yuhang@stargoto.com>
2017-06-16 17:42:53 +0800

    ..

---
+++ src/main/java/com/yu/hang/core/domain/note/TestNode.java
@@ -0,0 +1,22 @@
+package com.yu.hang.core.domain.note;
+
+public class TestNode {
+	private String xx;
+
+	public String getXx() {
+		return xx;
+	}
+
+	public void setXx(String xx) {
+		this.xx = xx;
+	}
+
+	@Override
+	public String toString() {
+		return "TestNode [xx=" + xx + "]";
+	}
+	
+	
+	
+
+}
